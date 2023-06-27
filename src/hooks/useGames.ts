1d966b9138ce2f6c5048b6034c06086b65690bb1
boogerjosh <ca@risas-MacBook-Air.local>
2023-06-27 10:42:00 +0700

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,18 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClients from "../services/api-clients";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platform }[];
+    metacritic: number;
   }
   
   interface FetchGamesResponse {
